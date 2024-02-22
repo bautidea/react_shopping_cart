9e9eb7434d7a3131e1b415594288e224e6b72d1f
bautidea <juanbautistadeangelis@gmail.com>
2024-02-22 09:22:55 -0300

    Developing Store page, changing elements type.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,19 @@
+import { Col, Row } from 'react-bootstrap';
+import storeItems from '../data/items.json';
+import StoreItem from '../components/StoreItem';
+
 export const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      {/* in mid size we have 2 cols, in extrasmall 1, and in large 3 */}
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
