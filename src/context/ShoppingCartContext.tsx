48c0758ced524573849c7e46adc55050e9029f1b
bautidea <juanbautistadeangelis@gmail.com>
2024-02-25 16:02:20 -0300

    Appling get item, and increase item quantity logic to Shopping Cart Context.

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,12 +1,24 @@
-import { ReactNode, createContext, useContext } from 'react';
+import { ReactNode, createContext, useContext, useState } from 'react';
 
 // Types.
 type ShoppingCartProviderProps = {
   children: ReactNode;
 };
 
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseQuantity: (id: number) => void;
+  decreaseQuantity: (id: number) => void;
+  removeQuantity: (id: number) => void;
+};
+
 // Constants.
-const ShoppingCartContext = createContext({});
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
 // Function for using Context.
 // Context lets the parent component make some information available to any
@@ -17,8 +29,30 @@ export function useShoppingCart() {
 
 // This function is used in App.tsx, this would be the parent component.
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  function increaseQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider value={{ getItemQuantity, increaseQuantity }}>
       {children}
     </ShoppingCartContext.Provider>
   );
