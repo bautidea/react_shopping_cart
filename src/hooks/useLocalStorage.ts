bf4c163f7c6b644b62843e153519908d7bac66c3
bautidea <juanbautistadeangelis@gmail.com>
2024-02-27 11:57:39 -0300

    Working with custom hook 'useLocalStorage'

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,1 +1,24 @@
-export function useLocalStorage() {}
+import { useEffect, useState } from 'react';
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  // As initial value we are going to use a function, because i only want
+  // to invoke this function one time, because its an slow operation.
+  // I dont want to execute the function every time the component re-renders.
+  const [value, setValue] = useState<T>(() => {
+    const jsonValue = localStorage.getItem(key);
+
+    if (jsonValue != null) return JSON.parse(jsonValue);
+
+    if (typeof initialValue === 'function') {
+      return (initialValue as () => T)();
+    } else {
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
